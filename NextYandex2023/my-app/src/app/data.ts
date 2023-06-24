e5edb77e268a0661c25d116de6dd23a7b18c7dbf
Vadim Cherepanov <xenos26@gmail.com>
2023-06-25 03:54:25 +0700

    chore: add data for education purpose

---
+++ NextYandex2023/my-app/src/app/data.ts
@@ -0,0 +1,20 @@
+export const filmDetails = {
+  id: 'asdjnwjenf2342njna',
+  title: 'Simpsons',
+  seasonsCount: 33,
+  genre: 'comedy',
+  similar: [
+    {
+      id: 'sa86f68as6f8as68',
+      title: 'South park',
+    },
+  ],
+  reviews: [
+    {
+      id: 'xc754vcx5',
+      author: 'SP',
+      text: 'Самый лучший мультфильм',
+      rating: 10,
+    },
+  ],
+}
