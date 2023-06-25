f8e24210d0ed4cb5b7a918b5f53a454588788560
Vadim Cherepanov <xenos26@gmail.com>
2023-06-26 06:58:27 +0700

    feat: add NewReviewForm

--- NextYandex2023/my-app/src/components/NewReviewForm/NewReviewForm.tsx
+++ NextYandex2023/my-app/src/components/NewReviewForm/NewReviewForm.tsx
@@ -1,16 +1,16 @@
 "use client"
-import { FunctionComponent, useReducer } from "react";
+import { FunctionComponent, useReducer } from "react"
 
 export const NewReviewForm: FunctionComponent = () => {
   const FORM_ACTIONS = {
     setName: "setName",
     setText: "setText",
-    setRating: "setRating"
+    setRating: "setRating",
   }
   const initialState = {
     name: "Vadim",
     text: "text",
-    rating: 10
+    rating: 10,
   }
   const reducer = (state, action) => {
     switch (action?.type) {
@@ -21,27 +21,36 @@ export const NewReviewForm: FunctionComponent = () => {
       case FORM_ACTIONS.setRating:
         return { ...state, rating: action.payload.rating }
       default:
-        return state;
+        return state
     }
   }
 
-}
-const [state, dispatch] = useReducer(reducer, initialState)
-const onNameChange = (event) => dispatch({ type: FORM_ACTIONS.setName, payload: { name: event.target.value } })
-return (
-  <div>
-    <label>
-      Name:
-      <input value={state.name} onChange={onNameChange} type="text" />
-    </label>
-    <label>
-      Text:
-      <input type="text" />
-    </label>
-    <label>
-      Rating:
-      <input type="number" />
-    </label>
-  </div>
-)
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const onNameChange = (event) =>
+    dispatch({
+      type: FORM_ACTIONS.setName,
+      payload: { name: event.target.value },
+    })
+  const onTextChange = (event) =>
+    dispatch({
+      type: FORM_ACTIONS.setText,
+      payload: { text: event.target.value },
+    })
+  const onRatingChange = (event) => dispatch({ type: FORM_ACTIONS.setRating, payload: { rating: event.target.value } })
+  return (
+    <div>
+      <label>
+        Name:
+        <input value={state.name} onChange={onNameChange} type="text" />
+      </label>
+      <label>
+        Text:
+        <input value={state.text} onChange={onTextChange} type="text" />
+      </label>
+      <label>
+        Rating:
+        <input value={state.rating} onChange={onRatingChange} type="number" />
+      </label>
+    </div>
+  )
 }
