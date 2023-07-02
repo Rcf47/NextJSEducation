0d4d1e097b4da71b1b676a22855928299e373c2e
Vadim Cherepanov <xenos26@gmail.com>
2023-07-02 09:34:44 +0700

    feat: add usePathname

--- my-experiments/src/app/layout.tsx
+++ my-experiments/src/app/layout.tsx
@@ -1,19 +1,24 @@
+'use client'
+import { usePathname } from 'next/navigation'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>Header</header>
+        <header>Header
+          {pathname !== '/' && <Link href='/'>Main</Link>}
+        </header>
         {children}
         <footer>Footer</footer>
       </body>
