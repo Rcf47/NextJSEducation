1cd6c6f7604208ed30de7166e763549cb7557d9c
Vadim Cherepanov <xenos26@gmail.com>
2023-07-02 09:13:15 +0700

    feat: add useRouter

--- my-experiments/src/app/page.tsx
+++ my-experiments/src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <>
       <h1>Hello</h1>
       <Link href="/selection">Selection is here</Link>
+      <br />
+      <button type='button' onClick={() => router.push('/selection')}>Click here let&apos;s go to selection</button>
     </>
   )
 }
