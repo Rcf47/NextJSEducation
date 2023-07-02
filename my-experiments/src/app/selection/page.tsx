18d6c94c9d19873faa2610bb392219f6f230121f
Vadim Cherepanov <xenos26@gmail.com>
2023-07-02 09:33:51 +0700

    feat: add router for selection

--- my-experiments/src/app/selection/page.tsx
+++ my-experiments/src/app/selection/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function Selection() {
+  const router = useRouter()
   return (
     <>
       <h2>title</h2>
       <div>Little selection here</div>
+      <Link href='/'>Back</Link>
+      <br />
+      <button type='button' onClick={() => router.push('/')}>Back with router</button>
     </>
   )
 }
