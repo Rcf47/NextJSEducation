06b05715644f4763947d3312aa06d66c7025f281
Vadim Cherepanov <xenos26@gmail.com>
2023-07-04 07:00:50 +0700

    chore: add fira code font

--- quick-start/app/layout.tsx
+++ quick-start/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Fira_Code } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const firaCode = Fira_Code({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={firaCode.className}>{children}</body>
     </html>
   )
 }
