8cf40be510040488699fb0169e536118dd2950a3
Vadim Cherepanov <xenos26@gmail.com>
2023-07-04 07:06:16 +0700

    chore: add some styles for button

--- quick-start/app/about/page.tsx
+++ quick-start/app/about/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
   return (
-    <h1 className="mb-4 text-4xl">This is about page</h1>
+    <>
+      <h1 className="mb-4 text-4xl">This is about page</h1>
+      <Link href="/" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800">To app root</Link>
+    </>
   )
 }
