b814a1ef6cd76c0c0662c5e21a75170b64afe3a9
Vadim Cherepanov <xenos26@gmail.com>
2023-07-05 19:34:49 +0700

    feat: add getData from jsonplaceholder

--- quick-start/app/blog/page.tsx
+++ quick-start/app/blog/page.tsx
@@ -1,5 +1,28 @@
-export default function Blog() {
+import { Metadata } from "next";
+import Link from "next/link";
+
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: { revalidate: 60 },
+  });
+
+  return response.json();
+}
+export const metadata: Metadata = {
+  title: "Blog Page",
+};
+export default async function Blog() {
+  const posts = await getData();
   return (
-    <h2>Lil blog here</h2>
-  )
+    <>
+      <h2 className="mb-4 text-2xl">Lil blog here</h2>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
