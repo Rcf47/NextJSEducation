2c2cbe7dce19e61ef35774eee5519b99c6ac77d5
Vadim Cherepanov <xenos26@gmail.com>
2023-07-05 19:35:26 +0700

    feat: add getData for Post

--- quick-start/app/blog/[id]/page.tsx
+++ quick-start/app/blog/[id]/page.tsx
@@ -3,8 +3,25 @@ type Props = {
     id: string
   }
 }
-export default function Post({ params: { id } }: Props) {
+
+async function getData(id: string) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: { revalidate: 60 },
+  });
+  return response.json()
+}
+export async function generateMetadata({ params: { id } }: Props) {
+  const post = await getData(id)
+  return {
+    title: post.title
+  }
+}
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id)
   return (
-    <h2>Post here {id}</h2>
+    <>
+      <h2 className="mb-4 text-2xl">{post.title}</h2>
+      <p>{post.body}</p>
+    </>
   )
 }
